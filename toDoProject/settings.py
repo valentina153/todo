a570e7b8f0836c6e71f12e8a8106576527434b62
valentina153 <tina_033@live.com>
2021-05-27 21:54:01 +0200

    git commit -m "settings"

--- toDoProject/settings.py
+++ toDoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gt)=x7!i4j5d-j!0(5fqt54dof9i@#+6%qhry*o-e835@mg+gc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
